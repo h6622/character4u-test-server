2740aee2c3238670dd38568156bac2f54bc13f65
h662 <godh662@gmail.com>
2020-02-11 06:52:34 +0900

    create login-form

---
+++ components/LoginForm.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" max-width="480px">
+      <template v-slot:activator="{ on }">
+        <a v-on="on" class="loginMargin">로그인</a>
+      </template>
+      <v-card>
+        <div class="closeBtn pt-3">
+          <v-btn @click="dialog = false" text>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </div>
+
+        <v-card-title class="justify-center">
+          <span class="loginTitle">로그인</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-container>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="아이디"
+                type="email"
+                required
+                outlined
+                clearable
+                color="purple darken-2"
+              />
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                label="비밀번호"
+                type="password"
+                required
+                outlined
+                clearable
+                color="purple darken-2"
+                hide-details="auto"
+              />
+              <v-row>
+                <v-col>
+                  <v-checkbox
+                    v-model="sessions"
+                    label="로그인 유지"
+                    color="purple darken-2"
+                    class="mt-0"
+                  />
+                </v-col>
+                <v-col class="mt-1 d-flex justify-end">
+                  아이디 / 비밀번호 찾기
+                </v-col>
+              </v-row>
+              <v-btn class="submitBtn" color="purple darken-2" type="submit"
+                >로그인</v-btn
+              >
+              <div class="signup mt-5">
+                아직 회원이 아니신가요?
+                <nuxt-link to="signup">회원가입</nuxt-link>
+              </div>
+            </v-container>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.closeBtn {
+  text-align: right;
+}
+
+.loginTitle {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.submitBtn {
+  width: 100%;
+  height: 56px !important;
+  color: white;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.signup {
+  text-align: center;
+}
+
+.loginMargin {
+  margin-top: 1px;
+}
+</style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,34 +5,81 @@
         <img
           src="@/assets/images/gnb-logo.png"
           alt="character4u"
-          class="navLogo mr-5"
+          class="navLogo"
         />
       </v-col>
-      <v-col cols="8" class="hidden-sm-and-down"
-        ><span v-for="item in menuItems" :key="item.path" class="mr-5">{{
-          item.title
-        }}</span></v-col
-      >
-      <v-col class="hidden-sm-and-down">
-        <span class="d-flex justify-end">로그인</span></v-col
-      >
+      <v-col cols="8" class="hidden-sm-and-down">
+        <ul>
+          <li v-for="item in menuItems" :key="item.path" class="pr-5 navMenu">
+            <nuxt-link :to="item.path">
+              {{ item.title }}
+            </nuxt-link>
+            <ul class="subItems">
+              <li
+                v-for="subItem in item.subItems"
+                :key="subItem.path"
+                class="pr-5 pt-3"
+              >
+                <nuxt-link :to="subItem.path">
+                  {{ subItem.subTitle }}
+                </nuxt-link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </v-col>
+      <v-col class="d-flex justify-end">
+        <span class="hidden-sm-and-down"><login-form /></span>
+        <span class="hidden-sm-and-down ml-5"
+          ><nuxt-link to="signup">회원가입</nuxt-link></span
+        >
+      </v-col>
       <v-col class="hidden-md-and-up">
-        <span class="d-flex justify-end">모바일</span></v-col
-      >
+        <span class="d-flex justify-end">모바일</span>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import LoginForm from '@/components/Loginform'
+
 export default {
+  components: {
+    LoginForm
+  },
   data() {
     return {
       menuItems: [
         { title: '공모전 개최', path: '/create' },
-        { title: '공모전 리스트', path: '/lists' },
-        { title: '공모전 우승작', path: '/win' },
+        {
+          title: '공모전 리스트',
+          path: '/lists',
+          subItems: [
+            { subTitle: '공모전 리스트', path: '/lists' },
+            { subTitle: 'My 공모전', path: '/mylists' },
+            { subTitle: '관심 공모전', path: '/favolate' }
+          ]
+        },
+        {
+          title: '공모전 우승작',
+          path: '/win',
+          subItems: [
+            { subTitle: '공모전 우승작', path: '/lists' },
+            { subTitle: '디자이너 포트폴리오', path: '/portfolio' },
+            { subTitle: '디자이너 랭킹', path: '/ranking' }
+          ]
+        },
         { title: '캐릭터포유 안내', path: '/info' },
-        { title: '고객센터', path: '/service' }
+        {
+          title: '고객센터',
+          path: '/qna',
+          subItems: [
+            { subTitle: '자주하는 질문', path: '/qna' },
+            { subTitle: '문의게시판', path: '/board' },
+            { subTitle: '공지사항', path: '/notice' }
+          ]
+        }
       ]
     }
   }
@@ -41,6 +88,25 @@ export default {
 
 <style lang="scss" scoped>
 .navLogo {
-  width: 7.5rem;
+  width: 120px;
+  height: 20px;
+}
+
+ul {
+  display: flex;
+  padding: 0;
+  li {
+    list-style-type: none;
+  }
+}
+
+.subItems {
+  position: absolute;
+  display: none;
+}
+
+.navMenu:hover .subItems {
+  position: absolute;
+  display: flex;
 }
 </style>
