f013eb67605cc75a5ece059ca342d04a051004e1
h662 <godh662@gmail.com>
2020-02-16 13:39:31 +0900

    create main how

--- components/main/MainHow.vue
+++ components/main/MainHow.vue
@@ -5,20 +5,31 @@
       <a :class="clickreq" @click="onClickReq">의뢰자</a>
       <a :class="clickdsg" @click="onClickDsg" class="marginLeft">디자이너</a>
     </div>
-    <div>
-      <span>1</span>
-      <span>2</span>
-      <span>3</span>
+    <div class="mainHowLists">
+      <main-how-card :step="step1" :title="title1" />
+      <main-how-card :step="step2" :title="title2" class="marginTop" />
+      <main-how-card :step="step3" :title="title3" />
     </div>
   </div>
 </template>
 
 <script>
+import MainHowCard from '@/components/main/MainHowCard'
+
 export default {
+  components: {
+    MainHowCard
+  },
   data() {
     return {
       clickreq: 'clicked',
-      clickdsg: 'disabled'
+      clickdsg: 'disabled',
+      step1: 'STEP1',
+      step2: 'STEP2',
+      step3: 'STEP3',
+      title1: '공모전 개최',
+      title2: '공모전 참여작 모집',
+      title3: '1위 선정 및 상금 지급'
     }
   },
   methods: {
@@ -41,7 +52,7 @@ export default {
 <style lang="scss" scoped>
 .bg {
   width: 100%;
-  height: 500px;
+  height: 700px;
   background: linear-gradient(rgba(20, 20, 20, 0.5), rgba(20, 20, 20, 0.5)),
     url('~assets/images/main-bg.jpg');
 }
@@ -81,5 +92,15 @@ export default {
 
 .marginLeft {
   margin-left: 40px;
+}
+
+.mainHowLists {
+  display: flex;
+  justify-content: center;
+  margin-top: 63px;
+}
+
+.marginTop {
+  margin: -10px 16px 0 16px;
 }
 </style>
---
+++ components/main/MainHowCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="mainHowCard">
+    <div class="mainHowImg" />
+    <h2 class="mainHowStep">{{ step }}</h2>
+    <h1 class="mainHowTitle">{{ title }}</h1>
+    <div class="mainHowContent">
+      캐릭터포유는 표시된 상금의 20%를(우승자 세금 포함) 수수료로 공제 후 80%를
+      디자이너에게 지급합니다. 해당 옵션은 수수료를 고객님이 대신 부담하여
+      상금의 100%가 디자이너에게 지급됩니다.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    step: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.mainHowCard {
+  width: 240px;
+  height: 398px;
+  border-radius: 5px;
+  border: solid 1px #d8d8d8;
+  background-color: #ffffff;
+}
+
+.mainHowImg {
+  width: 176px;
+  height: 91px;
+  margin: 29px 32px 22px 32px;
+  background-color: #757575;
+}
+
+.mainHowStep {
+  text-align: center;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 10px;
+  font-weight: bold;
+  color: $azure;
+}
+
+.mainHowContent {
+  width: 176px;
+  height: 140px;
+  margin: 22px 32px 52px 32px;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 14px;
+  line-height: 1.43;
+  color: #666666;
+}
+
+.mainHowTitle {
+  text-align: center;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  margin-top: 8px;
+}
+</style>
