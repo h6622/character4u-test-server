6817d28ded73f66b8a9bc301ff1b9a20aafd4386
h662 <godh662@gmail.com>
2020-02-16 15:11:23 +0900

    fix navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,46 +1,41 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <img
-          src="@/assets/images/gnb-logo.png"
-          alt="character4u"
-          class="navLogo"
-        />
-      </v-col>
-      <v-col cols="8" class="hidden-sm-and-down">
-        <ul>
-          <li v-for="item in menuItems" :key="item.path" class="pr-5 navMenu">
-            <nuxt-link :to="item.path">
-              {{ item.title }}
-            </nuxt-link>
-            <ul class="subItems">
-              <li
-                v-for="subItem in item.subItems"
-                :key="subItem.path"
-                class="pr-5 pt-3"
-              >
-                <nuxt-link :to="subItem.path">
-                  {{ subItem.subTitle }}
-                </nuxt-link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </v-col>
-      <v-col v-if="!me" class="d-flex justify-end">
-        <span class="hidden-sm-and-down mr-5"><login-form /></span>
-        <span class="hidden-sm-and-down ml-5"><signup-form /></span>
-      </v-col>
-      <v-col v-else class="d-flex justify-end">
+  <div class="container">
+    <div class="navLogoItems">
+      <img
+        src="@/assets/images/gnb-logo.png"
+        alt="character4u"
+        class="navLogo"
+      />
+      <ul class="marginLeft">
+        <li v-for="item in menuItems" :key="item.path" class="pr-5 navMenu">
+          <nuxt-link :to="item.path">
+            {{ item.title }}
+          </nuxt-link>
+          <ul class="subItems">
+            <li
+              v-for="subItem in item.subItems"
+              :key="subItem.path"
+              class="pr-5 pt-3"
+            >
+              <nuxt-link :to="subItem.path">
+                {{ subItem.subTitle }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <div v-if="!me" class="navLogoItems">
+        <span><login-form /></span>
+        <span class="marginLeft"><signup-form /></span>
+      </div>
+      <div v-else>
         <span>{{ me.name }}님 환영합니다!</span>
         <span><v-btn @click="onLogout">로그아웃</v-btn></span>
-      </v-col>
-      <v-col class="hidden-md-and-up">
-        <span class="d-flex justify-end">모바일</span>
-      </v-col>
-    </v-row>
-  </v-container>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -101,6 +96,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  padding: 25px 0;
+  width: 1610px;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.navLogoItems {
+  display: flex;
+}
+
 .navLogo {
   width: 120px;
   height: 20px;
@@ -133,5 +143,13 @@ a {
   &:focus {
     color: $azure;
   }
+}
+
+.marginLeft {
+  margin-left: 40px;
+}
+
+.test {
+  padding-right: 500px;
 }
 </style>
